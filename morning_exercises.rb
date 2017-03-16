fe382579af17afefe655186a5a6b6fb8e46b6702
wthoma22 <w.thomas390@yahoo.com>
2017-03-15 19:46:47 -0600

    "second commit"

--- morning_exercises.rb
+++ morning_exercises.rb
@@ -1,1 +1,20 @@
-hello
+number = 0
+sum = 0
+
+100.times do |x|
+    x += 1
+    number = sum += x
+end
+
+puts "The square of all sums is #{number ** 2}"
+
+sum_2 = 0
+
+100.times do |x|
+    y = (x + 1) ** 2
+    sum_2 += y
+end
+
+puts "The sum of all squares is #{sum_2}"
+
+puts "The differnece is #{number ** 2 - sum_2}"
