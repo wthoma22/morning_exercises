b628adf45a808a4e5f5496c7112733dec230296f
wthoma22 <w.thomas390@yahoo.com>
2017-03-21 09:21:46 -0600

    first commit

---
+++ morning_exercises_4.rb
@@ -0,0 +1,13 @@
+class AnimalReverse(animal)
+
+  def individual_letters
+    animal.split("")
+  end 
+  
+  def reverse_letters
+    individual_letters[0..2 = individual_letters[2..0]]
+  end 
+end 
+
+
+animal_1 = AnimalReverse.new('cow')
